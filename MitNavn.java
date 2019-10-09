9889ec112b294a28847e43ffe6c758206813b3da
1337k1ng <mb664@cphbusiness.dk>
2019-10-09 15:34:37 +0200

    Her har jeg ændret filen for at lave opgave 3

--- MitNavn.java
+++ MitNavn.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 system.out.println("Mit navn er Mads"); 
  system.out.println("Claes har rettet");
 system.out.println("Nu har Mads vaeret her igen");
+system.out.println("Hej Mads har skrevet her i bash");
 }
 }
