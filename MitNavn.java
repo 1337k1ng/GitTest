f2dbe916fb191e9fad36f58ce3cd62d31ab5150c
1337k1ng <54639915+1337k1ng@users.noreply.github.com>
2019-10-09 15:15:19 +0200

    Update MitNavn.java

--- MitNavn.java
+++ MitNavn.java
@@ -3,5 +3,6 @@ public class MitNavn {
 public static void main(String[] args) { 
 system.out.println("Mit navn er Mads"); 
  system.out.println("Claes har rettet");
+system.out.println("Nu har Mads vaeret her igen");
 }
 }
